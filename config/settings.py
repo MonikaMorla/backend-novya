c5a2bad6ecfd62768d473b748d078eee8f93c199
Monika Morla <111050564+MonikaMorla@users.noreply.github.com>
2025-11-17 11:52:12 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = [
     'novya-django-bze0g9hjegbve8dw.centralindia-01.azurewebsites.net',
     'localhost',
     '127.0.0.1',
-    '169.254.129.4',
+    'ALLOWED_HOSTS = ['*']',
     '.azurewebsites.net',
 ]
 
